2b6ff700f0e41a759a528022fcffe1ada8490119
mussabtafal <mussab_tafal@hotmail.com>
2022-07-03 14:21:57 +0300

    Manipulating test run ordering

---
+++ C# Testing/Calculations/CalculationsTests/TestCollectionOrderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AppTests
+{
+    public class TestCollectionOrderer : ITestCollectionOrderer
+    {
+        public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
+        {
+            return testCollections.OrderBy(x => x.DisplayName);
+        }
+    }
+}
