c3709073858f8e977d91dcda6295e297e37833cd
mussabtafal <mussab_tafal@hotmail.com>
2022-07-03 09:04:26 +0300

    Practicing XUnit Assertion Library

---
+++ C# Testing/Calculations/Calculations/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestClasses
+{
+    public class Customer
+    {
+        public string Name => "Aref";
+
+        public int GetOrdersByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Hello");
+            }
+            return 100;
+        }
+        public int Age => 35;
+    }
+
+    public class LoyalCustomer : Customer
+    {
+        public int Discount{ get; set;}
+
+        public LoyalCustomer()
+        {
+            Discount = 10;
+        }
+
+        public override int GetOrdersByName(string name)
+        {
+            return 101;
+        }
+    }
+
+    public static class CustomerFactory
+    {
+
+    }
+}
